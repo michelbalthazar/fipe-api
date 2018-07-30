0ecf4c66af75e055bc2fd919f16e26b95fdec267
Michel Balthazar <michel@nfe.io>
2018-07-30 19:49:19 -0300

    implemented common folder and some classes

---
+++ src/MBTech.Domain/Common/StringExtension.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace System
+{
+    public static class StringExtension
+    {
+        public static byte[] GetBytesFromString(this string value)
+        {
+            return Encoding.UTF8.GetBytes(value);
+        }
+
+        public static T JsonToObject<T>(this string json) where T : class
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        public static string ToJson<T>(this object value)
+        {
+            return JsonConvert.SerializeObject(value);
+        }
+    }
+}
