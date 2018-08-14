61c97afc53284942ba1a551682e9ecbbd292d811
Michel Balthazar <michel@nfe.io>
2018-08-14 09:22:23 -0300

    implement ISmallCarsUtilitiesClient

---
+++ src/MBTech.Domain/ISmallCarsUtilitiesClient.cs
@@ -0,0 +1,16 @@
+﻿using MBTech.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBTech.Domain
+{
+    public interface ISmallCarsUtilitiesClient
+    {
+        /// <summary>Obter todas as marcas de carro utilitário</summary>
+        /// <returns>Lista de todas as marcas de carros utilitário</returns>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        Task<Result<SmallCarsBrands>> GetBrandsAsync(System.Threading.CancellationToken cancellationToken);
+    }
+}
