b0009f1154d9a3b3ad6257a2af920e81ac2efa13
Michel Balthazar <michel@nfe.io>
2018-08-14 09:26:42 -0300

    implement testhelper and generateObjectToTest class

---
+++ tests/UnitTests/GenerateObjectToTest.cs
@@ -0,0 +1,19 @@
+﻿using MBTech.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests
+{
+    public static class GenerateObjectToTest
+    {
+        public static SmallCarsBrands SmallCarsBrands()
+        {
+            return new SmallCarsBrands
+            {
+                Brands = new List<SmallCarBrand> {new SmallCarBrand { Brand = "test", Code = "1" }, new SmallCarBrand { Brand = "test", Code = "2" }, new SmallCarBrand { Brand = "test", Code = "3" } },
+
+            };
+        }
+    }
+}
---
+++ tests/UnitTests/TestHelper.cs
@@ -0,0 +1,113 @@
+﻿using Moq;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    public static class TestHelper
+    {
+        #region mocks
+
+        public static Mock<HttpClient> CreateMockHttpGet(string response, HttpStatusCode httpStatusCode = HttpStatusCode.OK)
+        {
+            var responseMessage = new Mock<HttpResponseMessage>();
+            responseMessage.Object.StatusCode = httpStatusCode;
+            responseMessage.Object.Content = new FakeHttpContent(response);
+
+            var messageHandler = new FakeHttpMessageHandler(responseMessage.Object);
+
+            var mockHttpClient = new Mock<HttpClient>(messageHandler);
+
+            return mockHttpClient;
+        }
+
+        public static HttpClient CreateMockHttp(HttpMethod httpMethod, string response = "responseToTest", HttpStatusCode httpStatusCode = HttpStatusCode.OK)
+        {
+            var handler = new FakeHttpMessageHandler(response, httpMethod, httpStatusCode);
+
+            var mockHttpClient = new Mock<HttpClient>(handler);
+
+            return new HttpClient(handler);
+        }
+
+        public static HttpClient CreateMockHttp(HttpMethod httpMethod, byte[] response, HttpStatusCode httpStatusCode = HttpStatusCode.OK)
+        {
+            var handler = new FakeHttpMessageHandler(response, httpMethod, httpStatusCode);
+
+            var mockHttpClient = new Mock<HttpClient>(handler);
+
+            return new HttpClient(handler);
+        }
+
+        #endregion mocks
+
+    }
+
+    #region class to helper mock httpClient
+
+    public class FakeHttpMessageHandler : HttpMessageHandler
+    {
+        private HttpResponseMessage response;
+
+        public FakeHttpMessageHandler(string response, HttpMethod httpMethod, HttpStatusCode httpStatusCode)
+        {
+            this.response = SendAsync(response, httpMethod, httpStatusCode);
+        }
+
+        public FakeHttpMessageHandler(byte[] response, HttpMethod httpMethod, HttpStatusCode httpStatusCode)
+        {
+            this.response = SendAsync(response, httpMethod, httpStatusCode);
+        }
+
+        public FakeHttpMessageHandler(HttpResponseMessage response)
+        {
+            this.response = response;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var responseTask = new TaskCompletionSource<HttpResponseMessage>();
+            responseTask.SetResult(response);
+
+            return responseTask.Task;
+        }
+
+        public virtual HttpResponseMessage SendAsync(string response, HttpMethod httpMethod, HttpStatusCode httpStatusCode)
+        {
+            return new HttpResponseMessage(httpStatusCode) { RequestMessage = new HttpRequestMessage(httpMethod, "http://sample/test"), Content = new StringContent(response) };
+        }
+
+        public virtual HttpResponseMessage SendAsync(byte[] response, HttpMethod httpMethod, HttpStatusCode httpStatusCode)
+        {
+            return new HttpResponseMessage(httpStatusCode) { RequestMessage = new HttpRequestMessage(httpMethod, "http://sample/test"), Content = new ByteArrayContent(response) };
+        }
+    }
+
+    public class FakeHttpContent : HttpContent
+    {
+        public string Content { get; set; }
+
+        public FakeHttpContent(string content)
+        {
+            Content = content;
+        }
+
+        protected async override Task SerializeToStreamAsync(Stream stream, TransportContext context)
+        {
+            byte[] byteArray = Encoding.ASCII.GetBytes(Content);
+            await stream.WriteAsync(byteArray, 0, Content.Length);
+        }
+
+        protected override bool TryComputeLength(out long length)
+        {
+            length = Content?.Length ?? 0;
+            return true;
+        }
+    }
+
+    #endregion class to helper mock httpClient
+}
