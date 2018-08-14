1c00afe305882e4a5ef10d92381fc73cbe6efe68
Michel Balthazar <michel@nfe.io>
2018-08-14 09:22:08 -0300

    refactor string extensions

--- src/MBTech.Domain/Common/StringExtension.cs
+++ src/MBTech.Domain/Common/StringExtension.cs
@@ -5,19 +5,10 @@ namespace System
 {
     public static class StringExtension
     {
-        public static byte[] GetBytesFromString(this string value)
-        {
-            return Encoding.UTF8.GetBytes(value);
-        }
+        public static byte[] GetBytesFromString(this string value) => Encoding.UTF8.GetBytes(value);
 
-        public static T JsonToObject<T>(this string json) where T : class
-        {
-            return JsonConvert.DeserializeObject<T>(json);
-        }
+        public static T JsonToObject<T>(this string json) where T : class => JsonConvert.DeserializeObject<T>(json);
 
-        public static string ToJson<T>(this object value)
-        {
-            return JsonConvert.SerializeObject(value);
-        }
+        public static string ToJson<T>(this object value) => JsonConvert.SerializeObject(value);
     }
 }
